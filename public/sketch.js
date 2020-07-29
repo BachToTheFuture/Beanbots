f5f4d3a544e703d53ac4eb2ebdb5f67598f9d645
Glitch (code-bean-kamen) <none>
2020-07-29 03:27:46 +0000

    🗻🖖 Checkpoint ./public/index.html:18047618/1241 ./public/sketch.js:18047618/9970

--- public/sketch.js
+++ public/sketch.js
@@ -1,10 +1,5 @@
 
 /*
-sketch.js
-===============================================
-This is essentially the main JS file
-*/  
-
 var allSensors = [
   "ColorSensor", "DistanceSensor"
 ]
@@ -89,4 +84,51 @@ function drawField() {
   stroke(219, 91, 87);
   square(width-75, height-75, 80);
   pop();
+}
+*/
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+// Code for: https://youtu.be/urR596FsU68
+
+// module aliases
+var Engine = Matter.Engine,
+  // Render = Matter.Render,
+  World = Matter.World,
+  Bodies = Matter.Bodies;
+
+var engine;
+var world;
+var boxes = [];
+
+var ground;
+
+function setup() {
+  createCanvas(400, 400);
+  engine = Engine.create();
+  world = engine.world;
+  //Engine.run(engine);
+  var options = {
+    isStatic: true
+  }
+  ground = Bodies.rectangle(200, height, width, 100, options);
+
+  World.add(world, ground);
+}
+
+function mousePressed() {
+  boxes.push(new Box(mouseX, mouseY, random(10, 40), random(10, 40), `hsl(${Math.floor(random(0,360))}, 100%, 71%)`));
+}
+
+function draw() {
+  background(51);
+  Engine.update(engine);
+  for (var i = 0; i < boxes.length; i++) {
+    boxes[i].render();
+  }
+  noStroke(255);
+  fill(170);
+  rectMode(CENTER);
+  rect(ground.position.x, ground.position.y, width, 100);
+
 }
