da463b7f096e79ff6266e7e6a6d6719d0adafb2e
Glitch (code-bean-kamen) <none>
2020-07-30 21:13:13 +0000

    🚉🌦 Checkpoint ./public/index.html:18047618/1885 ./public/editor.js:18047618/3053

--- public/editor.js
+++ public/editor.js
@@ -213,26 +213,10 @@ var options = {
 	oneBasedIndex : true
 };
 
-var blocklyArea = document.getElementById('pills-profile');
-  var blocklyDiv = document.getElementById('blocklyDiv');
-  var workspace = Blockly.inject(blocklyDiv,options);
-  var onresize = function(e) {
-    // Compute the absolute coordinates and dimensions of blocklyArea.
-    var element = blocklyArea;
-    var x = 0;
-    var y = 0;
-    do {
-      x += element.offsetLeft;
-      y += element.offsetTop;
-      element = element.offsetParent;
-    } while (element);
-    // Position blocklyDiv over blocklyArea.
-    blocklyDiv.style.left = x + 'px';
-    blocklyDiv.style.top = y + 'px';
-    blocklyDiv.style.width = blocklyArea.offsetWidth + 'px';
-    blocklyDiv.style.height = blocklyArea.offsetHeight + 'px';
-    Blockly.svgResize(workspace);
-  };
-  window.addEventListener('resize', onresize, false);
-  onresize();
-  Blockly.svgResize(workspace);
+var workspace;
+workspace = Blockly.inject('editor',options);
+
+var workspaceBlocks = document.getElementById("workspaceBlocks"); 
+
+/* Load blocks to workspace. */
+Blockly.Xml.domToWorkspace(workspaceBlocks, workspace);
