9736ff1d4da8bd06e80d079dc4c692a890538f86
Glitch (code-bean-kamen) <none>
2020-07-30 17:18:11 +0000

    🍉💄 Checkpoint ./public/editor.js:18047618/457 ./public/index.html:18047618/6

--- public/editor.js
+++ public/editor.js
@@ -111,8 +111,24 @@ Blockly.JavaScript['wait'] = function(block) {
   return code;
 };
 
-var workspace = Blockly.inject('blocklyDiv',
-      {toolbox: document.getElementById('toolbox')});
+var options = { 
+	toolbox : document.getElementById('toolbox'), 
+	collapse : true, 
+	comments : true, 
+	disable : true, 
+	maxBlocks : Infinity, 
+	trashcan : false, 
+	horizontalLayout : false, 
+	toolboxPosition : 'start', 
+	css : true, 
+	media : 'https://blockly-demo.appspot.com/static/media/', 
+	rtl : false, 
+	scrollbars : false, 
+	sounds : true, 
+	oneBasedIndex : true
+};
+
+var workspace = Blockly.inject('blocklyDiv',options);
 /*
 
 // Type something!
