f666507ed44d4ae72cab89cc7011c43356302158
Glitch (code-bean-kamen) <none>
2020-07-30 20:39:57 +0000

    🏔🏝 Checkpoint ./public/index.html:18047618/226 ./public/editor.js:18047618/1013

--- public/editor.js
+++ public/editor.js
@@ -213,5 +213,26 @@ var options = {
 	oneBasedIndex : true
 };
 
-var workspace;
-workspace = Blockly.inject('editor',options);
+var blocklyArea = document.getElementById('blocklyArea');
+  var blocklyDiv = document.getElementById('blocklyDiv');
+  var workspace = Blockly.inject(blocklyDiv,options);
+  var onresize = function(e) {
+    // Compute the absolute coordinates and dimensions of blocklyArea.
+    var element = blocklyArea;
+    var x = 0;
+    var y = 0;
+    do {
+      x += element.offsetLeft;
+      y += element.offsetTop;
+      element = element.offsetParent;
+    } while (element);
+    // Position blocklyDiv over blocklyArea.
+    blocklyDiv.style.left = x + 'px';
+    blocklyDiv.style.top = y + 'px';
+    blocklyDiv.style.width = blocklyArea.offsetWidth + 'px';
+    blocklyDiv.style.height = blocklyArea.offsetHeight + 'px';
+    Blockly.svgResize(workspace);
+  };
+  window.addEventListener('resize', onresize, false);
+  onresize();
+  Blockly.svgResize(workspace);
